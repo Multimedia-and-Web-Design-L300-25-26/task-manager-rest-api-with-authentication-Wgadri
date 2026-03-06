b39ac847f680fb81a7a7afcbe77ed15c22e9117e
Wisdom Gadri <wisdomgadri01@gmail.com>
2026-03-06 19:00:02 +0000

    Initial commit

--- src/app.js
+++ src/app.js
@@ -1,16 +1,3 @@
-/*  import express from "express";
-import authRoutes from "./routes/authRoutes.js";
-import taskRoutes from "./routes/taskRoutes.js";
-
-const app = express();
-
-app.use(express.json());
-
-app.use("/api/auth", authRoutes);
-app.use("/api/tasks", taskRoutes);
-
-export default app;   */
-
 import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
